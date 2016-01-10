da23c8964c63ce4c705caaed5bf58bcbe1660873
Chris Orcutt <orcutt.chris@gmail.com>
2016-01-10 14:47:29 -0800

    Complete Adding Notes

--- MakeSchoolNotes/Controllers/DisplayNoteViewController.swift
+++ MakeSchoolNotes/Controllers/DisplayNoteViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class DisplayNoteViewController: UIViewController {
 
+  var note: Note?
+
+  @IBOutlet weak var noteTitleTextField: UITextField!
+  @IBOutlet weak var noteContentTextView: UITextView!
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    noteTitleTextField.text = ""
+    noteContentTextView.text = ""
+  }
+  
+
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
@@ -20,6 +33,14 @@ class DisplayNoteViewController: UIViewController {
         print("Cancel button tapped")
       } else if identifier == "Save" {
         print("Save button tapped")
+        let note = Note()
+        note.title = noteTitleTextField.text ?? ""
+        note.content = noteContentTextView.text
+        note.modificationTime = NSDate()
+        
+        let destinationViewController = segue.destinationViewController as! ListNotesTableViewController
+        destinationViewController.notes.append(note)
+        destinationViewController.tableView.reloadData()
       }
     }
   }
