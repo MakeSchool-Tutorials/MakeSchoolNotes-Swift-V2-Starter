459f31c9dddadc9bcedda97ffe1a54cc461d6293
Dion Larson <me@dionlarson.com>
2017-06-06 23:04:23 -0400

    Add NSDate

--- MakeSchoolNotes/Extensions/Date+convertToString.swift
+++ MakeSchoolNotes/Extensions/Date+convertToString.swift
@@ -13,3 +13,9 @@ extension Date {
         return DateFormatter.localizedString(from: self, dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.medium)
     }
 }
+
+extension NSDate {
+    func convertToString() -> String {
+        return DateFormatter.localizedString(from: (self as Date), dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.medium)
+    }
+}
